6dc89fc66f0c6f42d3315db0d1ce115bd1a8728c
Alexander <pchp250g8@tutamail.com>
2026-02-09 14:53:07 +0300

    Добавьте файлы проекта.

---
+++ SolveLinealEquation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveLinealEquation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Input coefficients of lineal equation");
+            float.TryParse(Console.ReadLine(), out float a);
+            float.TryParse(Console.ReadLine(), out float b);
+            Console.WriteLine("Try to solve the equation:{0}*x+{1}=0", a, b);
+            if ((a == 0) && (b == 0))
+            {
+                Console.WriteLine("The answer is any number");
+            }
+            else if ((a == 0) && (b != 0))
+            {
+                Console.WriteLine("The equation has no solution");
+            }
+            else
+            {
+                Console.WriteLine("The equation has the following solution:{0}", -b / a);
+            }
+            Console.Read();
+        }
+    }
+}
